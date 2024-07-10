62e771dc4b76e5c6e3a165d94f74fa2179803e68
bulbul32123 <bulbulhero63@gmail .com>
2024-07-10 22:35:27 +0600

    Portfolio upadated

--- src/components/learning/Learning.jsx
+++ src/components/learning/Learning.jsx
@@ -27,14 +27,20 @@ export default function Learning() {
         </div>
         <div className="w-full h-full">
           <Accordian>
-            <AccordianItem trigger="What is Daily Learning?" value={1} date='7 July 2024 '>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, corporis. Dolores totam repudiandae laboriosam ullam sed facere, fugiat eaque consequatur. Consequuntur iure impedit recusandae ab dignissimos architecto totam dolor deserunt!
+            <AccordianItem trigger="History of Node.js 📖🔖" value={1} date='7 July 2024 '>
+              The history of Node.js is fascinating! It all began in 2009, when Ryan Dahl created the first version of Node.js. He wanted to create a fast and lightweight runtime environment for JavaScript, and he chose to use the Google Chrome V8 JavaScript engine as the foundation for Node.js. The first release of Node.js was met with great enthusiasm by developers, and it quickly gained popularity as a platform for developing server-side applications.
+
+              Since the first release of Node.js, there have been many exciting developments. One of the most notable was the release of the Node Package Manager (NPM) in 2010. NPM is a command-line interface that makes it easy to install and manage packages (i.e., reusable pieces of code) for Node.js applications. This made it much easier for developers to share and reuse code, and it helped to fuel the growth of the Node.js ecosystem. Another major development was the release of Node.js v0.12 in 2014, which introduced significant performance improvements.
+
+
             </AccordianItem>
-            <AccordianItem trigger="What is Monthly Learning?" value={2} date='7 July 2024 '>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, corporis. Dolores totam repudiandae laboriosam ullam sed facere, fugiat eaque consequatur. Consequuntur iure impedit recusandae ab dignissimos architecto totam dolor deserunt!
+            <AccordianItem trigger="Introduction to Node.js 😄" value={2} date='8 July 2024 '>
+              Node.js is an open-source and cross-platform JavaScript runtime environment. It is a popular tool for almost any kind of project!
+              Node.js runs the V8 JavaScript engine, the core of Google Chrome, outside of the browser. This allows Node.js to be very performant.
+              A Node.js app runs in a single process, without creating a new thread for every request. Node.js provides a set of asynchronous I/O primitives in its standard library that prevent JavaScript code from blocking and generally, libraries in Node.js are written using non-blocking paradigms, making blocking behavior the exception rather than the norm.
             </AccordianItem>
-            <AccordianItem trigger="What is Weekly Learning?" value={3} date='7 July 2024 '>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, corporis. Dolores totam repudiandae laboriosam ullam sed facere, fugiat eaque consequatur. Consequuntur iure impedit recusandae ab dignissimos architecto totam dolor deserunt!
+            <AccordianItem trigger="Node.js Modules" value={3} date='9 July 2024 '>
+              Module in Node. js is a simple or complex functionality organized in single or multiple JavaScript files which can be reused throughout the Node. js application.
             </AccordianItem>
           </Accordian>
         </div>
